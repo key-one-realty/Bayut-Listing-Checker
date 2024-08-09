f734fab57164c394b5af54d643f099b8c286a85b
Harith Onigemo <onigemotosin@gmail.com>
2024-08-09 12:45:45 +0400

    chore: installing redis on server

--- config/database.php
+++ config/database.php
@@ -143,7 +143,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -157,6 +157,8 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'scheme' => 'tls',
+            'read_write_timeout' => -1,
         ],
 
         'cache' => [
